8036517d994632afea3832ce0c2e65c3894d3b1e
Kumonda221 <kumonda@foxmail.com>
2018-01-15 23:20:21 +0800

    结构调整 20180112

---
+++ src/org/kucro3/klink/functional/FunctionRegistry.java
@@ -0,0 +1,4 @@
+package org.kucro3.klink.functional;
+
+public class FunctionRegistry {
+}
---
+++ src/org/kucro3/klink/functional/Optional.java
@@ -0,0 +1,9 @@
+package org.kucro3.klink.functional;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Optional {
+}
