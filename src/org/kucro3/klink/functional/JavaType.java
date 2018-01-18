4cc6d7fb5042360387d77464824df4712d4b5ff7
Kumonda221 <kumonda@foxmail.com>
2018-01-18 20:37:06 +0800

    增量更新 20180118

--- src/org/kucro3/klink/functional/JavaType.java
+++ src/org/kucro3/klink/functional/JavaType.java
@@ -1,5 +1,22 @@
 package org.kucro3.klink.functional;
 
 @Typename("Java")
-public interface JavaType extends Type<Class<?>> {
+public final class JavaType implements Type<Class<?>> {
+    public static JavaType of(Class<?> type)
+    {
+        return new JavaType(type);
+    }
+
+    JavaType(Class<?> type)
+    {
+        this.type = type;
+    }
+
+    @Override
+    public Class<?> getType()
+    {
+        return type;
+    }
+
+    private final Class<?> type;
 }
--- src/org/kucro3/klink/functional/KlinkFunction.java
+++ src/org/kucro3/klink/functional/KlinkFunction.java
@@ -3,8 +3,10 @@ package org.kucro3.klink.functional;
 import org.kucro3.klink.Environment;
 import org.kucro3.klink.flow.Flow;
 
-public class KlinkFunction implements Callable {
-    KlinkFunction(Flow flow)
+import java.util.List;
+
+public class KlinkFunction implements Function {
+    public KlinkFunction(Flow flow)
     {
         this.flow = flow;
     }
@@ -13,6 +15,36 @@ public class KlinkFunction implements Callable {
     public void call(Environment env, Ref[] refs, CallInfo context)
     {
 
+    }
+
+    @Override
+    public String getIdentifier()
+    {
+        return null;
+    }
+
+    @Override
+    public int getParameterCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public <T extends Type> List<Parameter<T>> getParameters()
+    {
+        return null;
+    }
+
+    @Override
+    public <T extends Type> List<Parameter<T>> getEssentialParameters()
+    {
+        return null;
+    }
+
+    @Override
+    public <T extends Type> List<Parameter<T>> getOptionalParameters()
+    {
+        return null;
     }
 
     private final Flow flow;
