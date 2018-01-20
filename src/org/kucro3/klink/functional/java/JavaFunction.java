f517a561bcb03e1c43c0c784c70c8a7906fcd423
Kumonda221 <kumonda@foxmail.com>
2018-01-20 15:52:42 +0800

    增量更新 20180120

--- src/org/kucro3/klink/functional/java/JavaFunction.java
+++ src/org/kucro3/klink/functional/java/JavaFunction.java
@@ -5,37 +5,60 @@ import org.kucro3.klink.Klink;
 import org.kucro3.klink.functional.Function;
 import org.kucro3.klink.functional.Parameter;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class JavaFunction implements Function {
+    public JavaFunction(String identifier, Method method, Parameter<JavaType>[] params)
+    {
+        this.identifier = identifier;
+        this.method = method;
+        this.params = Arrays.asList(params);
+        this.essentialParams = new ArrayList<>();
+        this.optionalParams = new ArrayList<>();
+
+        int i = 0;
+        for(; i < params.length && !params[i].isOptional(); i++)
+            essentialParams.add(params[i]);
+        for(; i < params.length; i++)
+            optionalParams.add(params[i]);
+
+        this.essentialParams.trimToSize();
+        this.optionalParams.trimToSize();
+    }
+
     @Override
     public String getIdentifier()
     {
-        return null;
+        return identifier;
     }
 
     @Override
     public int getParameterCount()
     {
-        return 0;
+        return params.size();
     }
 
     @Override
     public List<Parameter<JavaType>> getParameters()
     {
-        return null;
+        return Collections.unmodifiableList(params);
     }
 
     @Override
-    public List<Parameter<?>> getEssentialParameters()
+    public List<Parameter<JavaType>> getEssentialParameters()
     {
-        return null;
+        return Collections.unmodifiableList(essentialParams);
     }
 
     @Override
-    public List<Parameter<?>> getOptionalParameters()
+    public List<Parameter<JavaType>> getOptionalParameters()
     {
-        return null;
+        return Collections.unmodifiableList(optionalParams);
     }
 
     @Override
@@ -43,4 +66,14 @@ public class JavaFunction implements Function {
     {
 
     }
+
+    private final String identifier;
+
+    private final Method method;
+
+    private final List<Parameter<JavaType>> params;
+
+    private final ArrayList<Parameter<JavaType>> essentialParams;
+
+    private final ArrayList<Parameter<JavaType>> optionalParams;
 }
--- src/org/kucro3/klink/functional/java/JavaHelper.java
+++ src/org/kucro3/klink/functional/java/JavaHelper.java
@@ -1,7 +1,10 @@
 package org.kucro3.klink.functional.java;
 
+import org.kucro3.klink.functional.*;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Collection;
-import java.util.Optional;
 
 public class JavaHelper {
     public static Collection<JavaFunction> exportFunctions(Class<?> clazz)
@@ -10,9 +13,105 @@ public class JavaHelper {
         return null;
     }
 
-    public static Optional<JavaFunction> exportFunction()
+    @SuppressWarnings("unchecked")
+    public static java.util.Optional<JavaFunction> exportFunction(Method method)
     {
-        // TODO
-        return null;
+        Export exportInfo = method.getAnnotation(Export.class);
+
+        if(exportInfo == null)
+            return java.util.Optional.empty();
+
+        int access = method.getModifiers();
+        boolean virtual = Modifier.isStatic(access);
+
+        if(!Modifier.isPublic(access))
+            return java.util.Optional.empty();
+
+        boolean frontCallInfo;
+        String identifier = exportInfo.value();
+        java.lang.reflect.Parameter[] params = method.getParameters();
+
+        if(params.length == 0)
+            return java.util.Optional.empty();
+
+        if(params[0].getType().equals(CallInfo.class))
+            frontCallInfo = true;
+        else if(params[params.length - 1].getType().equals(CallInfo.class))
+            frontCallInfo = false;
+        else
+            return java.util.Optional.empty();
+
+        Parameter<JavaType>[] generic = new Parameter[params.length - 1];
+
+        int start = frontCallInfo ? 1 : 0;
+        int end = frontCallInfo ? params.length - 1 : params.length - 2;
+        for(int i = start, j = 0; i < end; i++, j++)
+            generic[j] = new JavaParameter(params[i]);
+
+        int j = 0;
+        for(; j < generic.length && !generic[j].isOptional(); j++);
+        for(; j < generic.length; j++)
+            if(!generic[j].isOptional())
+                return java.util.Optional.empty();
+
+        return java.util.Optional.of(new JavaFunction(identifier, method, generic));
+    }
+
+    public static class JavaParameter implements Parameter<JavaType>
+    {
+        public JavaParameter(java.lang.reflect.Parameter param) {
+            Optional optional = param.getAnnotation(Optional.class);
+            Reference reference = param.getAnnotation(Reference.class);
+
+            this.optional = optional != null;
+            this.reference = reference != null;
+
+            if(this.reference)
+                this.type = new JavaType(reference.value());
+            else
+                this.type = new JavaType(param.getType());
+
+            this.name = param.getName();
+            this.param = param;
+        }
+
+        @Override
+        public JavaType getType()
+        {
+            return type;
+        }
+
+        @Override
+        public boolean isReference()
+        {
+            return reference;
+        }
+
+        @Override
+        public boolean isOptional()
+        {
+            return optional;
+        }
+
+        @Override
+        public String getName()
+        {
+            return name;
+        }
+
+        public java.lang.reflect.Parameter getParameter()
+        {
+            return param;
+        }
+
+        protected final boolean optional;
+
+        protected final boolean reference;
+
+        protected final String name;
+
+        protected final JavaType type;
+
+        protected final java.lang.reflect.Parameter param;
     }
 }
