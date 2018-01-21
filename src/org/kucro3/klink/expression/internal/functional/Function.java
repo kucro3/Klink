88550bc75703658f316ff1f92174cea3d9715d6b
Kumonda221 <kumonda@foxmail.com>
2018-01-21 20:58:40 +0800

    增量更新 20180121

--- src/org/kucro3/klink/expression/internal/functional/Function.java
+++ src/org/kucro3/klink/expression/internal/functional/Function.java
@@ -1,4 +1,16 @@
 package org.kucro3.klink.expression.internal.functional;
 
-public class Function {
+import org.kucro3.klink.Ref;
+import org.kucro3.klink.expression.ExpressionCompiler;
+import org.kucro3.klink.expression.ExpressionInstance;
+import org.kucro3.klink.expression.ExpressionLibrary;
+import org.kucro3.klink.flow.Flow;
+import org.kucro3.klink.syntax.Sequence;
+
+public class Function implements ExpressionCompiler.Level2 {
+    @Override
+    public ExpressionInstance compile(ExpressionLibrary lib, Ref[] refs, Sequence seq, Flow codeBlock)
+    {
+        final Flow flow = codeBlock == null ? new Flow() : codeBlock;
+    }
 }
